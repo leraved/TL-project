da1e6344e4189999e80b44b59fa60ca6bd71bfbf
tanya <tanya_konovalova_2000@bk.ru>
2019-05-17 17:14:09 +0300

    Ход компьютера не рандомный

--- buttons.py
+++ buttons.py
@@ -40,9 +40,10 @@ def buttons():
 def click(number): #номер клетки
     global player
     global computer
+    
     process(number)
     player.add(number-1)
-    art_intellect()
+    art_intellect(number)
         
 def process(number):
     if number == 1:
@@ -64,14 +65,213 @@ def process(number):
     elif number == 9:
         button[8].config(text = "X", bg = '#7FFFD4', state = DISABLED)
     
-def art_intellect():
-    A = c - player - computer
-    print(A)
-    b = int(random.choice(list(A)))
-    button[b].config(text = "0", bg = '#FF69B4', state = DISABLED)
-    computer.add(b)
-        
-        
+def art_intellect(number):
+    if number == 1:
+        if 1 in player and 2 not in computer and 2 not in player:
+            button[2].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(2)
+        elif 2 in player and 1 not in computer and 1 not in player:
+            button[1].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(1)
+        elif 4 in player and 8 not in computer and 8 not in player:
+            button[8].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(8)
+        elif 8 in player and 4 not in computer and 4 not in player:
+            button[4].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(4)
+        elif 3 in player and 6 not in computer and 6 not in player:
+            button[6].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(6)
+        elif 6 in player and 3 not in computer and 3 not in player:
+            button[3].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(3)
+        else:
+            A = c - player - computer
+            b = int(random.choice(list(A)))
+            button[b].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(b)
+            
+    if number == 2:
+        if 0 in player and 2 not in computer and 2 not in player:
+            button[2].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(2)
+        elif 2 in player and 0 not in computer and 0 not in player:
+            button[0].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(0)
+        elif 4 in player and 7 not in computer and 7 not in player:
+            button[7].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(7)
+        elif 7 in player and 4 not in computer and 4 not in player:
+            button[4].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(4)
+        else:
+            A = c - player - computer
+            b = int(random.choice(list(A)))
+            button[b].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(b)
+
+    if number == 3:
+        if 0 in player and 1 not in computer and 1 not in player:
+            button[1].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(1)
+        elif 1 in player and 0 not in computer and 0 not in player:
+            button[0].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(0)
+        elif 5 in player and 8 not in computer and 8 not in player:
+            button[8].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(8)
+        elif 8 in player and 5 not in computer and 5 not in player:
+            button[5].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(5)
+        elif 4 in player and 6 not in computer and 6 not in player:
+            button[6].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(6)
+        elif 6 in player and 4 not in computer and 4 not in player:
+            button[4].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(4)
+        else:
+            A = c - player - computer
+            b = int(random.choice(list(A)))
+            button[b].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(b)
+
+    if number == 4:
+        if 0 in player and 6 not in computer and 6 not in player:
+            button[6].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(6)
+        elif 6 in player and 0 not in computer and 0 not in player:
+            button[0].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(0)
+        elif 4 in player and 5 not in computer and 5 not in player:
+            button[5].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(5)
+        elif 5 in player and 4 not in computer and 4 not in player:
+            button[4].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(4)
+        else:
+            A = c - player - computer
+            b = int(random.choice(list(A)))
+            button[b].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(b)
+
+    if number == 5:
+        if 1 in player and 7 not in computer and 7 not in player:
+            button[7].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(7)
+        elif 7 in player and 1 not in computer and 1 not in player:
+            button[1].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(1)
+        elif 3 in player and 5 not in computer and 5 not in player:
+            button[5].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(5)
+        elif 5 in player and 3 not in computer and 3 not in player:
+            button[3].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(3)
+        elif 0 in player and 8 not in computer and 8 not in player:
+            button[8].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(8)
+        elif 8 in player and 0 not in computer and 0 not in player:
+            button[0].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(0)
+        elif 2 in player and 6 not in computer and 6 not in player:
+            button[6].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(6)
+        elif 6 in player and 2 not in computer and 2 not in player:
+            button[2].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(2)
+        else:
+            A = c - player - computer
+            b = int(random.choice(list(A)))
+            button[b].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(b)
+
+    if number == 6:
+        if 2 in player and 8 not in computer and 8 not in player:
+            button[8].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(8)
+        elif 8 in player and 2 not in computer and 2 not in player:
+            button[2].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(2)
+        elif 3 in player and 4 not in computer and 4 not in player:
+            button[4].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(4)
+        elif 4 in player and 3 not in computer and 3 not in player:
+            button[3].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(3)
+        else:
+            A = c - player - computer
+            b = int(random.choice(list(A)))
+            button[b].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(b)
+
+    if number == 7:
+        if 7 in player and 8 not in computer and 8 not in player:
+            button[8].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(8)
+        elif 8 in player and 7 not in computer and 7 not in player:
+            button[7].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(7)
+        elif 0 in player and 3 not in computer and 3 not in player:
+            button[3].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(3)
+        elif 3 in player and 0 not in computer and 0 not in player:
+            button[0].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(0)
+        elif 4 in player and 2 not in computer and 2 not in player:
+            button[2].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(2)
+        elif 2 in player and 4 not in computer and 4 not in player:
+            button[4].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(4)
+        else:
+            A = c - player - computer
+            b = int(random.choice(list(A)))
+            button[b].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(b)
+
+    if number == 8:
+        if 6 in player and 8 not in computer and 8 not in player:
+            button[8].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(8)
+        elif 8 in player and 6 not in computer and 6 not in player:
+            button[6].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(6)
+        elif 1 in player and 4 not in computer and 4 not in player:
+            button[4].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(4)
+        elif 4 in player and 1 not in computer and 1 not in player:
+            button[1].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(1)
+        else:
+            A = c - player - computer
+            b = int(random.choice(list(A)))
+            button[b].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(b)
+
+    if number == 9:
+        if 7 in player and 6 not in computer and 6 not in player:
+            button[6].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(6)
+        elif 6 in player and 7 not in computer and 7 not in player:
+            button[7].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(7)
+        elif 2 in player and 5 not in computer and 5 not in player:
+            button[5].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(5)
+        elif 5 in player and 2 not in computer and 2 not in player:
+            button[2].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(2)
+        elif 4 in player and 0 not in computer and 0 not in player:
+            button[0].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(0)
+        elif 0 in player and 4 not in computer and 4 not in player:
+            button[4].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(4)
+        else:
+            A = c - player - computer
+            b = int(random.choice(list(A)))
+            button[b].config(text = "0", bg = '#FF69B4', state = DISABLED)
+            computer.add(b)
         
         
         
